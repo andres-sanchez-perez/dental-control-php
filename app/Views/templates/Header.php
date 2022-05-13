ba64878d1c2660f595b8a69cc7f2d372b24ce98d
Andres Sanchez <andres.sanchez.perez@udla.edu.ec>
2022-05-13 09:33:00 -0500

    Base url dinamic

--- app/Views/templates/Header.php
+++ app/Views/templates/Header.php
@@ -13,15 +13,15 @@
     <script src="<?=base_url('/js/bootstrap.bundle.min.js');?>"></script>
     <script src="<?=base_url('/js/Site.js');?>"></script>
     <script src="<?=base_url('/js/select2.min.js');?>"></script> -->
-    <link rel="stylesheet" href="/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/css/site.css"/>
-    <link rel="stylesheet" href="/css/jquery-ui.min.css" />
-    <link rel="stylesheet" href="/css/select2.min.css" />
-    <script src="/js/jquery-3.5.1.min.js"></script>
-    <script src="/js/jquery-ui.min.js"></script>
-    <script src="/js/bootstrap.bundle.min.js"></script>
-    <script src="/js/Site.js"></script>
-    <script src="/js/select2.min.js"></script>
+    <link rel="stylesheet" href="//css/bootstrap.min.css" />
+    <link rel="stylesheet" href="//css/site.css"/>
+    <link rel="stylesheet" href="//css/jquery-ui.min.css" />
+    <link rel="stylesheet" href="//css/select2.min.css" />
+    <script src="//js/jquery-3.5.1.min.js"></script>
+    <script src="//js/jquery-ui.min.js"></script>
+    <script src="//js/bootstrap.bundle.min.js"></script>
+    <script src="//js/Site.js"></script>
+    <script src="//js/select2.min.js"></script>
 </head>
 <body>
 </nav>
