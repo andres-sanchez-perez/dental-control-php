2e2cf03c65c8f6006256029ddbb405593db6fe5a
Andres Sanchez <andres.sanchez.perez@udla.edu.ec>
2022-05-13 09:16:00 -0500

    Base url dinamic

--- app/Views/errors/error.php
+++ app/Views/errors/error.php
@@ -1,6 +1,7 @@
 <?=$headerError?>
 <div class="centered">
     <h1  class="text-danger ">Inicie sesión para poder realizar operaciones con el sistema</h1>
+    <?php echo base_url()?>
 </div>
 </body>
 </html>
